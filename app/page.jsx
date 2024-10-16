dd17450ca5472fbec97f9baf2af1bd8fa6c58774
bioornal <spezialichristian@gmail.com>
2024-10-16 09:52:31 -0300

    agrego loader

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 'use client'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Image from "next/image"
 
 import { motion } from 'framer-motion';
+import { DotLoader } from 'react-spinners';
 import ContactForm from '@/components/formContacto';
 import MapaUbicacion from '@/components/MapaUbicacion';
 import HeroSection from '../components/HeroSection.jsx';
@@ -12,6 +14,33 @@ import Atracttions from '../components/Atracttions.jsx';
 import Footer from '../components/Footer.jsx';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+
+        <div className="flex flex-col items-center justify-center h-screen bg-white">
+          <div className="mb-8">
+            <Image
+              src="https://fogon-food-ordering.s3.sa-east-1.amazonaws.com/IguazuFalls/HeroImages/LOGO_FONDO.png"
+              alt="Logo de Iguazú Falls Duplex & Lodge"
+              width={240}
+              height={240}
+              className="w-60 h-60"
+            />
+          </div>
+          <DotLoader color="#111f12" size={100} />
+        </div>
+      </>
+    );
+  }
 
   return (
     <div className="flex flex-col min-h-screen ">
