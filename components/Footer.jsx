78a77e16e0678902a15823f26eafdc6b75e6a567
bioornal <spezialichristian@gmail.com>
2024-11-23 11:12:01 -0300

    link a selva ok

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-gray-800 text-white py-8 font-sans">
       <div className="container mx-auto px-4 md:px-6">
         <div className="mt-8 pt-8 border-t border-green-700 text-center">
-          <p className="text-sm">&copy; 2024 Iguazu Falls Duplex & Lodge | <span className="italic font-normal text-green-400">Selva Digital Diseños.</span></p>
+          <p className="text-sm">&copy; 2024 Iguazu Falls Duplex & Lodge | <a href="https://selva-digital.vercel.app/" target="_blank" rel="noopener noreferrer" className="italic font-normal text-green-400 hover:text-green-300">Selva Digital Diseños.</a></p>
         </div>
       </div>
     </footer>
