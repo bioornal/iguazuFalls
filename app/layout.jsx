a7bce5822a03d8de9d9d705dcbaace8e91758f0d
bioornal <spezialichristian@gmail.com>
2025-01-26 15:52:22 -0300

    agrego tags google ads

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,26 @@
 import './globals.css'
+import Script from 'next/script'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16839244653"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16839244653');
+          `}
+        </Script>
+      </head>
       <body className="min-h-screen font-sans">
         {children}
       </body>
